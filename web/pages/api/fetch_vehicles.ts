7e5f04f22f70c2cd455c0baab33e3257e42055fa
Jason Brown <browniefed@gmail.com>
2021-02-14 16:03:47 -0800

    just return

--- web/pages/api/fetch_vehicles.ts
+++ web/pages/api/fetch_vehicles.ts
@@ -1,42 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { GraphQLClient, gql } from "graphql-request";
 import { VehicleResponse } from "../../types";
 
 const VEHICLE_URL = "http://developer.trimet.org/ws/v2/vehicles";
-
-const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : `http://pdxlivebus.com`;
-const REFETCH_URL = `${BASE_URL}/api/fetch_vehicles`;
-
-const upsertVehicles = gql`
-  mutation InsertVehicles($vehicles: [vehicles_insert_input!]!) {
-    insert_vehicles(
-      objects: $vehicles
-      on_conflict: {
-        constraint: vehicles_pkey
-        update_columns: [routeNumber, direction, latitude, longitude, type]
-      }
-    ) {
-      affected_rows
-    }
-  }
-`;
-
-const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT, {
-  headers: {
-    "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
-  },
-});
-
-const wait = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, 7000);
-  });
-};
 
 const getVehicles = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(
@@ -55,31 +20,9 @@ const getVehicles = async (req: NextApiRequest, res: NextApiResponse) => {
         type: String(vehicle.type),
       };
     });
-
-    client.request(upsertVehicles, {
-      vehicles,
-    });
+    res.json(vehicles);
   }
-
-  await wait();
-  console.log("Refetching");
-  console.log(REFETCH_URL);
-
-  fetch(REFETCH_URL, {
-    method: "GET",
-  })
-    .then((f) => {
-      return f.json();
-    })
-    .then((d) => {
-      console.log(REFETCH_URL);
-      console.log(d);
-      res.json({ succes: true });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.json({ succes: true });
-    });
+  res.json([]);
 };
 
 export default getVehicles;
